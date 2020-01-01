705e824d4a31c67b6278e023905f2fcbe86c946c
Christoph Gohlke <cgohlke@uci.edu>
2019-12-31 22:12:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # psf/setup.py
 
 """Psf package setuptools script."""
@@ -17,11 +16,11 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
-readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from', code,
-                   re.MULTILINE | re.DOTALL).groups()[0]
+readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+                   code, re.MULTILINE | re.DOTALL).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)]
-                   + readme.splitlines()[1:])
+readme = '\n'.join([description, '=' * len(description)] +
+                   readme.splitlines()[1:])
 
 license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
                     re.MULTILINE | re.DOTALL).groups()[0]
@@ -30,6 +29,7 @@ license = license.replace('# ', '').replace('#', '')
 
 if 'sdist' in sys.argv:
     with open('LICENSE', 'w') as fh:
+        fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
     with open('README.rst', 'w') as fh:
         fh.write(readme)
@@ -52,9 +52,9 @@ setup(
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
-    python_requires='>=2.7',
-    install_requires=['numpy>=1.11.3'],
-    setup_requires=['setuptools>=18.0', 'numpy>=1.11.3'],
+    python_requires='>=3.6',
+    install_requires=['numpy>=1.14.5'],
+    setup_requires=['setuptools>=18.0', 'numpy>=1.14.5'],
     cmdclass={'build_ext': build_ext},
     packages=['psf'],
     ext_modules=[Extension('psf._psf', ['psf/psf.c'])],
@@ -69,10 +69,7 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: C',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
