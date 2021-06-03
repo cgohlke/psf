4262a5e1dd98198b562faf5b4df6808322187ded
Christoph Gohlke <cgohlke@uci.edu>
2021-06-03 12:42:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,14 +16,21 @@ version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
 
 description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
 
-readme = re.search(r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
-                   code, re.MULTILINE | re.DOTALL).groups()[0]
+readme = re.search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}__version__',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
 
-readme = '\n'.join([description, '=' * len(description)] +
-                   readme.splitlines()[1:])
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
 
-license = re.search(r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""', code,
-                    re.MULTILINE | re.DOTALL).groups()[0]
+license = re.search(
+    r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+    code,
+    re.MULTILINE | re.DOTALL,
+).groups()[0]
 
 license = license.replace('# ', '').replace('#', '')
 
@@ -37,10 +44,12 @@ if 'sdist' in sys.argv:
 
 class build_ext(_build_ext):
     """Delay import numpy until build."""
+
     def finalize_options(self):
         _build_ext.finalize_options(self)
         __builtins__.__NUMPY_SETUP__ = False
         import numpy
+
         self.include_dirs.append(numpy.get_include())
 
 
@@ -52,9 +61,14 @@ setup(
     author='Christoph Gohlke',
     author_email='cgohlke@uci.edu',
     url='https://www.lfd.uci.edu/~gohlke/',
-    python_requires='>=3.6',
-    install_requires=['numpy>=1.14.5'],
-    setup_requires=['setuptools>=18.0', 'numpy>=1.14.5'],
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/psf/issues',
+        'Source Code': 'https://github.com/cgohlke/psf',
+        # 'Documentation': 'https://',
+    },
+    python_requires='>=3.7',
+    install_requires=['numpy>=1.15.1'],
+    setup_requires=['setuptools>=18.0', 'numpy>=1.15.1'],
     cmdclass={'build_ext': build_ext},
     packages=['psf'],
     ext_modules=[Extension('psf._psf', ['psf/psf.c'])],
@@ -70,8 +84,9 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: C',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
