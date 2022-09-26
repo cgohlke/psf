a7fbf188ae8483e10eb3a43fa56d80135685ab4e
Christoph Gohlke <cgohlke@cgohlke.com>
2022-09-26 15:23:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,10 @@ class build_ext(_build_ext):
 
     def finalize_options(self):
         _build_ext.finalize_options(self)
-        __builtins__.__NUMPY_SETUP__ = False
+        if isinstance(__builtins__, dict):
+            __builtins__['__NUMPY_SETUP__'] = False
+        else:
+            setattr(__builtins__, '__NUMPY_SETUP__', False)
         import numpy
 
         self.include_dirs.append(numpy.get_include())
