15659970e426d5614e0de82ad1bcab715c5b8be0
Christoph Gohlke <cgohlke@cgohlke.com>
2025-09-20 10:44:45 -0700

    Update psf/__init__.py

--- psf/__init__.py
+++ psf/__init__.py
@@ -3,15 +3,7 @@
 from .psf import *
 from .psf import __all__, __doc__, __version__
 
+# constants are repeated for documentation
 
-def _set_module() -> None:
-    """Set __module__ attribute for all public objects."""
-    globs = globals()
-    module = globs['__name__']
-    for item in __all__:
-        obj = globs[item]
-        if hasattr(obj, '__module__'):
-            obj.__module__ = module
-
-
-_set_module()
+__version__ = __version__
+"""Psf version string."""
