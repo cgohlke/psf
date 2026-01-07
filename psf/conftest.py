1c74dc8ade43b7ab5038c3a9bfdd760cad2118fb
Christoph Gohlke <cgohlke@cgohlke.com>
2026-01-06 17:55:07 -0800

    Update psf/conftest.py

--- psf/conftest.py
+++ psf/conftest.py
@@ -5,7 +5,7 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-def doctest_config(doctest_namespace):
+def doctest_config(doctest_namespace) -> None:
     """Add numpy to doctest namespace."""
     numpy.set_printoptions(suppress=True, precision=5)
     doctest_namespace['numpy'] = numpy
